0eac158dd6872fafe49e0f48f5e38928b27cc9f2
Denisa Alicia Rissa <denisa.rissa@LEGO.com>
2025-12-01 09:04:34 +0100

    feat: Day 1 part 2

---
+++ day01_part2.py
@@ -0,0 +1,29 @@
+## Part 2
+input = open("day01_input.txt").read().split('\n')
+
+position = 50
+password = 0
+
+for rotation in input:
+    direction = rotation[0]
+    distance = int(rotation[1:])
+
+    if direction == 'L':
+        for turn in range(0,distance):
+            if position == 0:
+                position = 99
+            else:
+                position -= 1
+            if position == 0:
+                password += 1
+            
+    else:
+        for turn in range(0,distance):
+            if position == 99:
+                position = 0
+            else:
+                position += 1
+            if position == 0:
+                password += 1
+
+print(password)
